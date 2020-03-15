a3b96b6431c0c331521c006eea1ad096dd2cdcdc
Szymon Zmilczak <szymon.zmilczak@gmail.com>
2020-03-15 22:36:21 +0100

    feat(ci): Added release pipeline

--- setup.py
+++ setup.py
@@ -1,20 +1,42 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open('cms_forms/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+    else:
+        version = '0.0.1'
+
+with open("requirements.txt") as f:
+    _requirements = [line.split("#")[0].strip() for line in f]
+    install_requires = [req for req in _requirements if req and not req.startswith("-r")]
+
 
 setuptools.setup(
     name="cms_forms",
-    version="0.0.1",
-    author="Sim1234",
+    version=version,
+    author="Szymon Zmilczak",
     author_email="szymon.zmilczak@gmail.com",
+    maintainer="Szymon Zmilczak",
+    maintainer_email="szymon.zmilczak@gmail.com",
     description="Set of Django CMS plugins for creating forms",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sim1234/cms_forms",
+    license='MIT',
     packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    keywords=["django", "django cms", "cms", "forms"],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
